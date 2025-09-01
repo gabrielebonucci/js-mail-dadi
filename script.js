ffbd617878128d8ff5cd25dfdc9c5d872e909850
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-01 16:04:24 +0200

    pensiero logico per esercizio email

---
+++ script.js
@@ -0,0 +1,19 @@
+//creo una lista di email con gli invitati
+
+const invitedPeople = [
+    "mario.rossi@email.com",
+    "luigi.verdi@email.com",
+    "gianni.bianchi@email.com",
+    "gino.gialli@email.com",
+    "marco.neri@email.com",
+    "giuseppe.verde@email.com",
+    "gabriele.bonucci@email.com",
+]
+
+//chiedo all'utente la sua email
+
+//Creo una variabile per sapere se l'utente è invitato, la imposto su "false", diventerà "true solo se l'email è presente tra gli invitati"
+
+//Controllo ogni email nella lista con un ciclo for
+
+//dopo il ciclo, configuro il messaggio in base a "true" o "false"
